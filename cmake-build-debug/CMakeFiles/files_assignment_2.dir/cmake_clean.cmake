17f4ee8d7f98ba8df7dc82027bb05dd58de63744
Adham Magdy <60877873+AdhamMagdyA@users.noreply.github.com>
2021-12-23 19:48:02 +0200

    basic structures

---
+++ cmake-build-debug/CMakeFiles/files_assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/files_assignment_2.dir/main.cpp.obj"
+  "files_assignment_2.exe"
+  "files_assignment_2.exe.manifest"
+  "files_assignment_2.pdb"
+  "libfiles_assignment_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/files_assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Record{
+    int iKey;
+    int iVal;
+    // default constructor
+    Record(){}
+    // parameterized constructor
+    Record(int key, int val){
+        iKey = key;
+        iVal = val;
+    }
+};
+
+struct Block{
+    Record record[5];
+};
+
+
+int main() {
+    Record r1(10,2);
+    //writing to a file
+    ofstream outfile("blocks.bin", ios::app | ios::binary);
+    outfile.seekp(0);
+    outfile.write( reinterpret_cast<char *>(&r1), sizeof(r1) );
+
+    Record r;
+    //reading from a file
+    ifstream infile("blocks.bin", ios::binary);
+    infile.seekg(0);
+    infile.read( reinterpret_cast<char *>(&r), sizeof(r) );
+
+    cout << r.iKey << " " << r.iVal;
+
+
+}
